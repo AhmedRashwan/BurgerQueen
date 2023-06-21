bfc91788a9e5ed82641182c6eb951799472a85b7
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2023-06-22 02:32:17 +0300

    fix/images issues

--- src/stores/FoodItemsStore.js
+++ src/stores/FoodItemsStore.js
@@ -1,46 +1,77 @@
 import {defineStore} from "pinia"
 import {computed, reactive, ref} from "vue";
+import foodImage1 from "../assets/images/f1.png";
+import foodImage2 from "../assets/images/f2.png";
+import foodImage3 from "../assets/images/f3.png";
+import foodImage4 from "../assets/images/f4.png";
+import foodImage5 from "../assets/images/f5.png";
+import foodImage6 from "../assets/images/f6.png";
+import foodImage7 from "../assets/images/f7.png";
+import foodImage8 from "../assets/images/f8.png";
+import foodImage9 from "../assets/images/f9.png";
+
 
 export const useFoodItemsStore = defineStore('foodItems', () => {
     const activeItem = ref('all')
     const items = reactive([{
         name: "Amazing Pizza",
         type: "Pizza",
-        image: "src/assets/images/f1.png",
+        image: foodImage1,
         description: "Amzaing Pizza Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
-        price: 2,
+        price: 10,
     }, {
-        name: "Delicious Pasta",
+        name: "Delicious Burger",
         type: "Burger",
-        image: "src/assets/images/f2.png",
+        image: foodImage2,
         description: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
-        price: 2
+        price: 14
     }, {
         name: "hot Pizza",
         type: "Pizza",
-        image: "src/assets/images/f3.png",
+        image: foodImage3,
         description: "Hot Pizza Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
-        price: 2
+        price: 7
     }, {
         name: "Delicious Pasta",
         type: "Pasta",
-        image: "src/assets/images/f4.png",
+        image: foodImage4,
         description: "Delicious Pasta Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque ",
-        price: 2
+        price: 13
     },
         {
             name: "hot Fries",
             type: "Fries",
-            image: "src/assets/images/f5.png",
+            image: foodImage5,
             description: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
-            price: 2
+            price: 9
         },
         {
             name: "hot Pizza",
             type: "Pizza",
-            image: "src/assets/images/f6.png",
+            image: foodImage6,
             description: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
-            price: 2
+            price: 11
+        },
+        {
+            name: "Delicious Pasta",
+            type: "Burger",
+            image: foodImage7,
+            description: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+            price: 5
+        },
+        {
+            name: "Delicious Pasta",
+            type: "Burger",
+            image: foodImage8,
+            description: "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+            price: 5
+        },
+        {
+            name: "Delicious Pasta",
+            type: "Pasta",
+            image: foodImage9,
+            description: "Delicious Pasta Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque ",
+            price: 6
         }
     ]);
     const categorized = computed(() => {
--- src/stores/Offers.js
+++ src/stores/Offers.js
@@ -1,30 +1,33 @@
 import {defineStore} from "pinia"
 import {reactive} from "vue";
 
+import offer1 from "../assets/images/o1.jpg";
+import offer2 from "../assets/images/o2.jpg";
+
 export const useOfferStore = defineStore('offer', () => {
     const offers = reactive([
         {
-            image: "/src/assets/images/o1.jpg",
+            image: offer1,
             details: "offer 1",
+            link: "https://www.google.com",
+            percentage: 10
+        },
+        {
+            image: offer2,
+            details: "offer 2",
             link: "https://www.google.com",
             percentage: 20
         },
         {
-            image: "src/assets/images/o2.jpg",
-            details: "offer 2",
-            link: "https://www.google.com",
-            percentage: 30
-        },
-        {
-            image: "src/assets/images/o2.jpg",
+            image: offer2,
             details: "offer 3",
             link: "https://www.google.com",
-            percentage: 40
+            percentage: 30
         }, {
-            image: "src/assets/images/o2.jpg",
+            image: offer1,
             details: "offer 4",
             link: "https://www.google.com",
-            percentage: 30
+            percentage: 40
         }])
 
     function addOffer(offer) {
