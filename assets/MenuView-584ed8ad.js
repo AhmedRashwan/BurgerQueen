008d3c19b37207c007155b72e9554e0f74920188
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2023-06-22 03:46:28 +0300

    restructure the header

---
+++ assets/MenuView-584ed8ad.js
@@ -0,0 +1,1 @@
+import{o as a,c as s,d as e,F as _,e as c,f as n,h as t}from"./index-8b2111a8.js";const i={__name:"MenuView",setup(r){return(o,m)=>(a(),s(_,null,[e(c),e(n),e(t)],64))}};export{i as default};
